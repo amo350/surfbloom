279e1dd0ac3b1de85c2fb4ccba2adca2d78364a1
amo350 <amorales23am@gmail.com>
2026-01-18 11:34:54 -0600

    ai integration

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,15 @@
 import { inngest } from "@/inngest/client";
 import { createTRPCRouter, protectedProcedure } from "../init";
 import { prisma } from "@/lib/prisma";
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
+
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async()=> {
+    await inngest.send({
+      name: 'execute/ai'})
+      return { success: true, message: "Ai Request" };
+  }),
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
