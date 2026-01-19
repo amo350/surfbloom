d7965ffcb36fbeee96d3f28ffa25a57d1cc2cb3e
amo350 <amorales23am@gmail.com>
2026-01-19 12:20:38 -0600

    sidebar-layout

---
+++ src/app/(dashboard)/(org)/credentials/[credentialId]/page.tsx
@@ -0,0 +1,19 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+type PageProps = {
+    params: Promise<{
+        credentialId: string;
+    }>
+}
+
+
+
+const CredentialId = async ({params}: PageProps) => {
+  await requireAuth();
+    const {credentialId} = await params;
+  return (
+    <div>CredentialId: {credentialId}</div>
+  )
+}
+
+export default CredentialId
---
+++ src/app/(dashboard)/(org)/workflows/page.tsx
@@ -0,0 +1,11 @@
+import { requireAuth } from '@/lib/auth-utils'
+import React from 'react'
+
+const WorkFlowPage = async() => {
+  await requireAuth();
+  return (
+    <div>WorkFlowPage</div>
+  )
+}
+
+export default WorkFlowPage
